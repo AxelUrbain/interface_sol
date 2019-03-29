41d3db4f891d1b79dcc2fd3c1e8e6916cb4702a8
AxelUrbain <46811011+AxelUrbain@users.noreply.github.com>
2019-03-29 12:34:54 +0100

    Add files via upload

--- add_fly.php
+++ add_fly.php
@@ -4,7 +4,59 @@ if(!isset($_SESSION['login'])){
   header('Location: index.php');
   exit();
 }
- ?>
+
+if(isset($_POST['submit']))
+{
+  // Vérifie si le fichier a été uploadé sans erreur.
+if(isset($_FILES["FileImport"]) && $_FILES["FileImport"]["error"] == 0){
+    $nomOrigine = $_FILES["FileImport"]["name"];
+    $filetype = $_FILES["FileImport"]["type"];
+    $filesize = $_FILES["FileImport"]["size"];
+
+    // Vérifie l'extension du fichier
+    $elementChemin = pathinfo($nomOrigine);
+    $extensionFichier = $elementChemin['extension'];
+    $extensionAutorise = array("fc", "txt", "png");
+
+    if(!(in_array($extensionFichier, $extensionAutorise))) {
+      echo "Erreur : Veuillez sélectionner un format de fichier valide.";
+    }
+    else{
+      // Vérifie la taille du fichier - 5Mo maximum
+      $maxsize = 5 * 1024 * 1024;
+      if($filesize > $maxsize){
+        echo "Error: La taille du fichier est supérieure à la limite autorisée.";
+      }
+      else{
+            //Télécharger le fichier
+            // Copie dans le repertoire du script avec un nom
+            // incluant l'heure a la seconde pres
+            // PROBLEME DE DESTINATION A REGLER EN FIN DE PROJET !!!!
+            $repertoireDestination = dirname(__FILE__)."/";
+            //$repertoireDestination = '/fc';
+            $nomDestination = "fichier_du_".date("YmdHis").".".$extensionFichier;
+            if(move_uploaded_file($_FILES["FileImport"]["tmp_name"],$repertoireDestination.$nomDestination)){
+              echo "ca marche ! ";
+            }
+            else{
+              echo "Le fichier ne s'est pas téléchargé";
+           }
+      }
+    }
+    // Ouvre le fichier en lecture
+    //$fp = fopen($filename,"r");
+
+    //Parcourir le fichier
+
+    //Stocker les informations dans la base de données information_vol
+
+    //Supprimer le fichier
+
+} else{
+    echo "Error: " . $_FILES["FileImport"]["error"];
+}
+} ?>
+
 <!doctype html>
 <html lang="fr">
   <?php include('include/membre/header.php'); ?>
@@ -16,7 +68,8 @@ if(!isset($_SESSION['login'])){
         <h2 class="form-title">Ajouter un vol</h2>
         <form class="form-group" action="add_fly.php" method="post" enctype="multipart/form-data">
           <center>
-            <input class="btn btn-danger" type="file" name="FileImport" id="FileImport">
+            <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
+            <input class="btn btn-danger" type="file" name="FileImport">
             <input class="btn btn-success" type="submit" name="submit" value="Upload File">
           </center>
         </form>
--- function/delete_vol.php
+++ function/delete_vol.php
@@ -15,13 +15,21 @@ if(isset($_POST['delete_vol'])){
   // Connexion à la base de données
   $bdd = new PDO('mysql:host=localhost;dbname=interface_sol;charset=utf8', 'root', '');
   $id = $_GET['id'];
-  $query = $bdd->prepare();
 
-  //FAIRE LE SUITE LA SEMAINE PRO 
-  echo "ca marche !";
+  $queryVols = $bdd->prepare("DELETE FROM vols WHERE id = ".$id."");
+  $queryInfVols = $bdd->prepare("DELETE FROM information_vol WHERE id_Vol = ".$id."");
+
+  $queryVols->execute();
+  $queryInfVols->execute();
+
+  $queryVols->closeCursor();
+  $queryInfVols->closeCursor();
+
+  header('Location: ../PanelAdmin/hist_adm.php');
+
 }
 else {
-  echo "ca marche pas !";
+  echo "ERREUR : Les informations sélectionné son éronés ou vous n'avez pas suivi la procédure !";
 }
 
 ?>
